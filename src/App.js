c0c69ee15968a6ac8abb85eb92d7e4570327e55a
sevennoob <sevennoob0917@gmail.com>
2023-07-15 22:36:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Count from "./containers/Count"; //引入的Count的容器组件
+import Person from "./containers/Person"; //引入的Person的容器组件
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Count />
+        <hr />
+        <Person />
+      </div>
+    );
+  }
 }
-
-export default App;
